3b59fb613faabcd35298a6a34ac144cff2ef4ff2
Robert World <64225836+sabrtraveler@users.noreply.github.com>
2022-04-02 13:22:59 -0400

    finished thought-routes.js

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+
+const {
+  getAllThought,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction
+} = require('../../controllers/thought-controller');
+
+// SETUP the GET all and POST 
+router
+  .route('/')
+  .get(getAllThought)
+  .post(createThought);
+
+// SETUP the GET one, PUT and DELETE 
+router
+  .route('/:id')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router
+  .route('/:thoughtId/reactions')
+  .post(createReaction);
+
+  router
+  .route('/:thoughtId/reactions/:reactionId')
+  .delete(deleteReaction);
+
+module.exports = router;
